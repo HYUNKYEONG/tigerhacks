262de4a2cb4827026bafe9e6488d2bdb9aad638e
HYUNKYEONG <68999470+HYUNKYEONG@users.noreply.github.com>
2023-11-05 07:29:01 -0600

    Add files via upload

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import { Gate } from '@/components/gete';
+
+export default function Home() {
+  return (
+    <div className="w-full h-full flex">
+      <div className="items-center justify-center">
+        <Gate />
+      </div>
+    </div>
+  );
+}
